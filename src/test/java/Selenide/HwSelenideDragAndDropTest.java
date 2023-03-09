6db18e4d1018a8554793868d442958625841c2c0
Ïlya Safonov <Safonov.ILL@yandex.ru>
2023-03-09 11:33:52 +0300

    Added package selenide and homework for GitHub and Drag and Drop

---
+++ src/test/java/Selenide/HwSelenideDragAndDropTest.java
@@ -0,0 +1,35 @@
+package Selenide;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class HwSelenideDragAndDropTest {
+    @BeforeAll
+    static void beforeAll()  {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void practiceDragAndDrop(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldNotHave(text("A"));
+        sleep(7000);
+
+    }
+    @Test
+    void practiceDragAndDropTwo(){
+        //Тест падает
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(300, 0).release().perform();
+        $("#column-a").shouldNotHave(text("A"));
+
+    }
+
+}
---
+++ src/test/java/Selenide/HwSelenideTwoTest.java
@@ -0,0 +1,33 @@
+package Selenide;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class HwSelenideTwoTest {
+
+    @BeforeAll
+    static void beforeAll() {
+
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void practiceTestGithubTwo() {
+
+        open("https://github.com/");
+
+        $(".header-menu-wrapper").find(byText("Solutions")).hover();
+        $(byTagAndText("a","Enterprise")).click();
+        $(".application-main").shouldHave(text("Build like the best"));
+
+
+        sleep(6000);
+    }
+
+}
