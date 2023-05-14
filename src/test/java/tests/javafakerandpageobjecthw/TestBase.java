e78c2bab38d55438f8aa06a5805d2356b632aee8
Ïlya Safonov <Safonov.ILL@yandex.ru>
2023-05-14 12:17:45 +0300

    Added Config capabilities

--- src/test/java/tests/javafakerandpageobjecthw/TestBase.java
+++ src/test/java/tests/javafakerandpageobjecthw/TestBase.java
@@ -3,6 +3,9 @@ package tests.javafakerandpageobjecthw;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 @Tag("HomeWork")
 
@@ -11,8 +14,19 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "100.0";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         Configuration.pageLoadStrategy = "eager";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
+
     }
 }
