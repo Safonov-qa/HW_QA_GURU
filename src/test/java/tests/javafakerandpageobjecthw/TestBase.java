4d58abd7be3a5dabe640704bad747a975d3c4c4b
Ïlya Safonov <Safonov.ILL@yandex.ru>
2023-05-14 17:13:52 +0300

    Added Attach video

--- src/test/java/tests/javafakerandpageobjecthw/TestBase.java
+++ src/test/java/tests/javafakerandpageobjecthw/TestBase.java
@@ -43,6 +43,6 @@ public class TestBase {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
-       // Attach.addVideo();
+        Attach.addVideo();
     }
 }
