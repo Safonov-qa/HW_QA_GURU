2c0bce0d8563018714c1b38f0c3d610d507a4f02
Ïlya Safonov <Safonov.ILL@yandex.ru>
2023-05-14 16:41:53 +0300

    Added Attach.java - screen and page

--- src/test/java/tests/javafakerandpageobjecthw/TestBase.java
+++ src/test/java/tests/javafakerandpageobjecthw/TestBase.java
@@ -2,7 +2,9 @@ package tests.javafakerandpageobjecthw;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
@@ -35,5 +37,12 @@ public class TestBase {
     @BeforeEach
     void addListener() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+       // Attach.addVideo();
     }
 }
