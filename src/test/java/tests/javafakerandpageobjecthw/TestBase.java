4b30c00388a508ed4e136df4bd1a09b537a60658
Ïlya Safonov <Safonov.ILL@yandex.ru>
2023-05-15 13:31:26 +0300

    Added properties and helpers

--- src/test/java/tests/javafakerandpageobjecthw/TestBase.java
+++ src/test/java/tests/javafakerandpageobjecthw/TestBase.java
@@ -18,11 +18,11 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.browser = "chrome";
-        Configuration.browserVersion = "100.0";
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browserSize = System.getProperty("browser_size","1920x1080");
+        Configuration.browser = System.getProperty("browser","chrome");
+        Configuration.browserVersion = System.getProperty("browser_version","100.0");
+        Configuration.baseUrl = System.getProperty("url","https://demoqa.com");
+        Configuration.remote = "https://user1:1234@" + System.getProperty("selenoid_url", "selenoid.autotests.cloud/wd/hub");
         Configuration.pageLoadStrategy = "eager";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
